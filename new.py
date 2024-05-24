01c8911087458fd4c04b155f1c3a8257d78592d8
undying.muse <161160763+undying-muse@users.noreply.github.com>
2024-05-24 19:47:09 +0300

    Update new.py

--- new.py
+++ new.py
@@ -1,1 +1,7 @@
-yay
+# ok then this is my first actual time using github to actually code. 
+#Don't really know how this "branch" stuff works either so I'm just gonna throw what I know out here 
+
+#sooo.. can I do python functions? 
+print("hello world") 
+#seems like it 
+
